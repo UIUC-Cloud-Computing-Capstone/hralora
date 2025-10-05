654b33c84c5bce03b9ad0f9085b2b4bf5778104f
Liam Lian <liamyaolian@gmail.com>
2025-10-05 14:25:25 -0400

    resolve mac dependency issue

--- utils/log_utils.py
+++ utils/log_utils.py
@@ -6,14 +6,12 @@ def set_log_path(args):
     log path for different datasets and methods
     '''
     path =  './log/' + args.dataset +'/' + args.model + '/' + args.method + '/'
-    if not os.path.exists(path):
-        os.makedirs(path)
+    os.makedirs(path, exist_ok=True)
     
     path_log = os.path.join(path, args.config_name.split('.')[0])
     
     now = datetime.datetime.now()
     timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
     everything_record_path = path_log + '_' + str(timestamp)
-    if not os.path.exists(everything_record_path):
-        os.makedirs(everything_record_path)
+    os.makedirs(everything_record_path, exist_ok=True)
     return everything_record_path
