a9055195fcb30aa6961c2f6fe27ef83e2636760c
Liam Lian <liamyaolian@gmail.com>
2025-11-09 18:07:57 -0500

    refactor into method: _get_no_weight_lora

--- algorithms/solver/local_solver.py
+++ algorithms/solver/local_solver.py
@@ -39,10 +39,8 @@ class LocalUpdate(object):
         ddp_kwargs = DistributedDataParallelKwargs(find_unused_parameters=True)
         accelerator = Accelerator(kwargs_handlers=[ddp_kwargs])
 
-        if isinstance(getattr(args, 'heterogeneous_group'+str(hete_group_id)+'_lora'), list):
-            no_weight_lora = list(set(range(args.lora_layer)) - set(getattr(args, 'heterogeneous_group'+str(hete_group_id)+'_lora')))
-        elif isinstance(getattr(args, 'heterogeneous_group'+str(hete_group_id)+'_lora'), int):
-            no_weight_lora = list(set(range(args.lora_layer)) - set(args.block_ids_list[client_real_id]))
+        layer_max_rank_budget = getattr(args, 'heterogeneous_group'+str(hete_group_id)+'_lora')
+        no_weight_lora = self._get_no_weight_lora(args, client_real_id, layer_max_rank_budget)
 
         # early stop for exclusive training
         if len(no_weight_lora) == args.lora_layer:
@@ -132,6 +130,13 @@ class LocalUpdate(object):
 
         # optimizer.zero_grad()
         return accelerator.unwrap_model(model).state_dict(), np.mean(total_loss), no_weight_lora
+
+    def _get_no_weight_lora(self, args, client_real_id, layer_max_rank_budget):
+        if isinstance(layer_max_rank_budget, list):
+            no_weight_lora = list(set(range(args.lora_layer)) - set(layer_max_rank_budget))
+        elif isinstance(layer_max_rank_budget, int):
+            no_weight_lora = list(set(range(args.lora_layer)) - set(args.block_ids_list[client_real_id]))
+        return no_weight_lora
 
     def lora_tuning_extradata_ft(self, model, ldr_train, args, client_index=0):
         ddp_kwargs = DistributedDataParallelKwargs(find_unused_parameters=True)
