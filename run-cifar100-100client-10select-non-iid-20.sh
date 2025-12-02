5263258a5b1a0668f3f5b85aa86ec0241a84737e
youye <19175790+youyexie@users.noreply.github.com>
2025-12-01 23:52:44 -0800

    add 100-10 noniid case

---
+++ run-cifar100-100client-10select-non-iid-20.sh
@@ -0,0 +1,28 @@
+##############
+## CIFAR100 ##
+##############
+
+# exclusive
+NCCL_DEBUG=INFO TORCH_DISTRIBUTED_DETAIL=DEBUG accelerate launch --main_process_port 29505 main.py --config_name 'experiments/cifar100-100client-10select/Exclusive/Exclusive-rank24-noniid-pat_20_dir.yaml' 
+
+# fedhello
+NCCL_DEBUG=INFO TORCH_DISTRIBUTED_DETAIL=DEBUG accelerate launch --main_process_port 29505 main.py --config_name 'experiments/cifar100-100client-10select/FedHello/FedHello_noniid-pat_20_dir-noprior-s50-e50.yaml' 
+
+#fedit
+NCCL_DEBUG=INFO TORCH_DISTRIBUTED_DETAIL=DEBUG accelerate launch --main_process_port 29505 main.py --config_name 'experiments/cifar100-100client-10select/FedIT/FedIT-noniid-pat_20_dir.yaml' 
+
+#ffa-lora
+NCCL_DEBUG=INFO TORCH_DISTRIBUTED_DETAIL=DEBUG accelerate launch --main_process_port 29505 main.py --config_name 'experiments/cifar100-100client-10select/FFA-LoRA/FFA-LORA-image-b-only-noniid-20.yaml' 
+
+#LEGEND
+NCCL_DEBUG=INFO TORCH_DISTRIBUTED_DETAIL=DEBUG accelerate launch --main_process_port 29505 main.py --config_name 'experiments/cifar100-100client-10select/LEGEND/LEGEND-noniid-20.yaml' 
+
+#lokr
+NCCL_DEBUG=INFO TORCH_DISTRIBUTED_DETAIL=DEBUG accelerate launch --main_process_port 29505 main.py --config_name 'experiments/cifar100-100client-10select/LOKR/LOKR-noniid-20.yaml' 
+
+#ours
+NCCL_DEBUG=INFO TORCH_DISTRIBUTED_DETAIL=DEBUG accelerate launch --main_process_port 29505 main.py --config_name 'experiments/cifar100-100client-10select/Ours/alternating-training-warm20-double-rank-int-no-rank-vary-noniid-20.yaml' 
+
+#straggler
+NCCL_DEBUG=INFO TORCH_DISTRIBUTED_DETAIL=DEBUG accelerate launch --main_process_port 29505 main.py --config_name 'experiments/cifar100-100client-10select/Straggler/straggler-rank24-noniid-pat_20_dir.yaml' 
+
